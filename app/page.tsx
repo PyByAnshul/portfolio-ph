88287a6f8512484b28230ef099602025e5013c4d
PyByAnshul <a9756549615@gmail.com>
2025-11-29 20:04:10 +0530

    [UPDATE] project and resume

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Experience />
         <Education />
         <Projects />
-        <Achievements />
+        {/* <Achievements /> */}
         <Contact />
       </main>
     </div>
